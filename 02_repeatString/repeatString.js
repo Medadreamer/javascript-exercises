dfcb89f3a5b3468914c576502f820ef223fbbfb2
Youssef <choukriyoussef7905@gmail.com>
2022-06-16 20:18:09 +0100

    Complete repeatString() function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) {
+        return 'ERROR';
+    } 
+    else if(num === 0) {
+        return '';
+    }
+    let stringCopy = string
+    for(let i = 1; i < num; i++) {
+        stringCopy += string;
+    }
+    return stringCopy;
 };
 
 // Do not edit below this line
