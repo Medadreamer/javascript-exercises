98db496b922784c65791b2f9d8f6a47845402de9
Youssef <choukriyoussef7905@gmail.com>
2022-06-16 21:36:10 +0100

    Complete sumAll() function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if( a < 0 || b < 0 || typeof(a) !== 'number' || typeof(b) !== 'number') {
+        return 'ERROR'
+     }
 
+    let result = 0;
+    for(let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
