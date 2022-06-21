1d26c2da81dc7544d8878a6f45eba9fc61d17afb
Youssef <choukriyoussef7905@gmail.com>
2022-06-21 10:34:22 +0100

    Solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const peopleWithAge = people.map(person => {
+        if(!('yearOfDeath' in person)) {
+            
+            person['yearOfDeath'] = new Date().getFullYear();
+        }
+        age = person.yearOfDeath - person.yearOfBirth;
+        return {name: person.name, age: age}
+    })
 
+    TheOldest = peopleWithAge.sort((a, b) =>{
+        if(a.age > b.age) {
+            return -1;
+        }
+
+        return 1;
+    });
+
+    return TheOldest[0];
 };
 
 // Do not edit below this line
