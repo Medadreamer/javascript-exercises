e5849ca3d93022e9680a50ebaf0b9e688ecedd0e
Youssef <choukriyoussef7905@gmail.com>
2022-06-21 09:15:12 +0100

    Solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,35 @@
-const palindromes = function () {
+const reverseString = function(string) {
+    let reversedString = [];
+    for(let i = string.length; i >= 0; i--) {
+        if(string[i] !== undefined) {
+            reversedString.push(string[i]);
 
+        }
+    }
+    return reversedString.join('');
+};
+
+
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    const specialChars = [',', '.', '!', '?', ' '];
+    let stringArray = string.split('');
+    
+    for (let i = 0; i < stringArray.length; i++) {
+        for (let j = 0; j < specialChars.length; j++) {
+            if (stringArray[i] === specialChars[j]) {
+                stringArray.splice(i, 1);
+                i -= 1;
+                break;
+            }
+        }
+        
+    }
+
+    const reversedString = reverseString(stringArray.join(''));
+
+
+    return reversedString == stringArray.join('');
 };
 
 // Do not edit below this line
