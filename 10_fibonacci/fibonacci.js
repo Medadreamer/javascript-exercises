764105498a2a4b36cfa2f46e5a3d1bdf55273558
Youssef <choukriyoussef7905@gmail.com>
2022-06-21 09:36:40 +0100

    Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index < 1) {
+        return 'OOPS'
+    }
+    const series = [1, 1];
+    let number = 0;
+    for (let i = 1; i <= index; i++) {
+        number = series[series.length - 1] + series[series.length - 2];
+        series.push(number);
+        
+    }
+    return series[index - 1];
 };
 
 // Do not edit below this line
